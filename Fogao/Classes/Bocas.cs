b5fca5eccd1972c712669776c55620924adea3d9
Matheus Marcelo <matheuscrudi095@gmail.com>
2026-01-30 22:34:05 -0300

    Correçoes sutis no metodo boca

--- Fogao/Classes/Bocas.cs
+++ Fogao/Classes/Bocas.cs
@@ -1,37 +1,42 @@
 namespace classes;
 
 class Boca
-{
-    public int PotenciaMaxima {get; private set;}
+        //definindo as propriedades da boca do fogao
+{   public int PotenciaMaxima {get; private set;} 
     public int PotenciaAtual {get; private set;}
     public bool Estado {get; private set;}
 
-    public Boca (int potenciaMaxima)
+        //inicializa a classe com a boca desligada, solicitando um valor para a potencia maxima (que sera usado posteriormente) e usa um metodo para tratar este valor, definindo a potencia maxima sempre entre 1-10
+    public Boca (int potenciaInserida)
     {
-        this.PotenciaMaxima = ArredondadorDePotencia(potenciaMaxima);        
+        this.PotenciaAtual = 0;
+        this.PotenciaMaxima = ArredondadorDePotencia(potenciaInserida, 10);        
         this.Estado = false;
     }
 
-    private int ArredondadorDePotencia (int num)
+        //metodo responsavel por arredondar o valor de potencia fornecido 
+    private int ArredondadorDePotencia (int num, int valorMaximo)
     {
-        if ((num >= 1) && (num <= 10)) return num;
-        else if (num > 10) return 10;
+        if ((num >= 1) && (num <= valorMaximo)) return num;
+        else if (num > valorMaximo) return valorMaximo;
         else if (num <= 1) return 1;
         return 1;
     }
 
-    public void Ascender (int potencia)
+        //metodo que ascende a boca, alterando o estado da mesma, respeitando os limites fisicos do hardware
+    public void Acender (int potencia)
     {
         if (!this.Estado)
         {
             this.Estado = true;
-            this.PotenciaAtual = ArredondadorDePotencia(potencia);
+            this.PotenciaAtual = ArredondadorDePotencia(potencia, PotenciaMaxima);
         } else
         {
             Console.WriteLine("Esta boca ja esta acesa");
         }
     }
 
+        //metodo para desligar a boca, alterando suas propriedades de estado e potencia
     public void Apagar()
     {
         if (this.Estado)
